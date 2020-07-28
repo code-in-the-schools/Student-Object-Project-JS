8b61d6710d06c943059778f5a3e3d86b695ff620
Zachiah Zook <zakhazook1@gmail.com>
2020-07-28 14:56:24 +0000

    added Student Object

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-console.log("Hello World!")
-var x = 3
-var z = 5
-console.log (x+z)
-console.log(5*3)
+console.log("Zachiah's Experiments in JavaScript");
+
+var x = 3;
+var z = 5;
+console.log (x+z);
+
+console.log(5*3);
+
+var txt1 = "What a very ";
+txt1 += "nice day!";
+console.log(txt1);
+
+var rectangle = {
+  width: 20, height: 50
+}
+console.log(rectangle);
+
+var b = 2020;
+var c = "July "
+console.log (c + b)
+console.log (Date())
+
+console.log("Student Object")
+ 
+
+var person = { 
+  name: "Zachiah Zook",
+  age: 14,
+  address: "3449 Falls Rd, Baltimore, MD",
+  gpa: 4.0,
+
+};
+
+console.log(person);
+
+console.log(person.name + " is " + person.age + " years old.");
+console.log("She lives at " + person.address + " and her GPA is " + person.gpa);
